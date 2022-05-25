bc4cf0d49929d2917b46f186397a12ab368ebf4a
zhangjingqiang <zhangjingqiang@gmail.com>
2022-05-25 20:41:41 +0900

    change order

--- macOS/setup.sh
+++ macOS/setup.sh
@@ -2,22 +2,19 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+brew install --cask iterm2
 brew tap homebrew/cask-fonts
 brew install --cask font-hack-nerd-font
-
+brew install --cask fig
+brew install zsh
 brew install git
 brew install tig
-brew install zsh
 brew install vim
 brew install --cask macvim
 brew install --cask emacs
-brew install --cask iterm2
-brew install --cask fig
 
-brew install docker
 brew install --cask drawio
 brew install --cask postman
 
 brew install --cask slack
 brew install --cask discord
-
