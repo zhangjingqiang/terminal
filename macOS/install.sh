3f3ed87993a152e67c0cea5ebc1135d2b2eb4a43
zhangjingqiang <zhangjingqiang@gmail.com>
2023-11-23 23:29:47 +0900

    Update install.sh

--- macOS/install.sh
+++ macOS/install.sh
@@ -6,10 +6,10 @@ brew tap homebrew/cask-fonts
 brew install --cask font-hack-nerd-font
 brew install --cask iterm2
 brew install --cask fig
-brew install powerlevel10k
 brew install tree
 brew install tmux
 brew install zsh
+brew install powerlevel10k
 brew install git
 brew install tig
 brew install jq
