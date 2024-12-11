52c7c7fca39face30c1303e61d9e6ff3d051a08e
zhangjingqiang <zhangjingqiang@gmail.com>
2024-12-11 22:59:39 +0900

    Update install.sh

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -6,7 +6,6 @@ sudo apt-get install tree -y
 sudo apt-get install vim -y
 sudo apt-get install emacs-gtk -y
 sudo apt-get install zsh -y
-brew install powerlevel10k
 sudo apt-get install tmux -y
 sudo apt-get install git -y
 sudo apt-get install tig -y
@@ -19,7 +18,7 @@ sudo apt update
 sudo apt install yq -y
 sudo apt-get install httpie -y
 curl -sS https://webinstall.dev/curlie | bash
-# curl -sS https://starship.rs/install.sh | sh
+curl -sS https://starship.rs/install.sh | sh
 sudo snap install k9s
 
 sudo snap install drawio
