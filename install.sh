9f060d67521f6a317eb2fbb2f764fed2160d290a
zhangjingqiang <zhangjingqiang@gmail.com>
2025-10-20 13:34:12 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install --cask font-hack-nerd-font
-# brew install powerlevel10k
-# brew install starship
 brew install tmux
 brew install zsh
 brew install vim
